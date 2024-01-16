35a25e098a65f34d9de3c56f3cfa09e8e2d72f9e
EC2 Default User <ec2-user@ip-172-31-9-160.ec2.internal>
2024-01-16 09:46:09 +0000

    Upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+ dockerpath=ninorainman/tsudevops:projectML
 
 # Step 2:  
 # Authenticate & tag
+docker login -u ninorainman
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ninorainman/tsudevops:projectML
