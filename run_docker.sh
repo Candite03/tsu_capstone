a558b38257fe4b359abe17f3d6aeff09be6be566
EC2 Default User <ec2-user@ip-172-31-11-4.ec2.internal>
2024-01-16 04:04:50 +0000

    Docker build

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build -t ninorainman/tsudevops:projectML .
 # Step 2: 
 # List docker images
-
+docker images
 # Step 3: 
 # Run flask app
+docker run --name py-app -p 80:80 ninorainman/tsudevops:projectML
