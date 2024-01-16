07703b6519a21c5a626c10549e1171abbe0f49ef
EC2 Default User <ec2-user@ip-172-31-9-160.ec2.internal>
2024-01-16 13:42:34 +0000

    Deploy to K8s cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,14 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath=ninorainman/tsudevops:projectML
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+  kubectl create deploy predictions-app --image=ninorainman/tsudevops:projectML
 # Step 3:
 # List kubernetes pods
-
+  kubectl get deploy,rs,svc,pods
 # Step 4:
 # Forward the container port to a host
-
+  kubectl port-forward pod/predictions-app --address 0.0.0.0 80:80
