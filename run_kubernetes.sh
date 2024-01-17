e1828be611ef0528ef2ec88709adc6cf7d2b78be
Candite03 <146722646+Candite03@users.noreply.github.com>
2024-01-17 10:23:01 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,10 +8,10 @@
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-  kubectl create deploy predictions-app --image=ninorainman/tsudevops:projectML
+  kubectl create deploy predictions-app --image=ninorainman/tsudevops:projectML -- /agnhost netexec --http-port=80
 # Step 3:
 # List kubernetes pods
-  minikube kubectl get deploy,rs,svc,pods
+  kubectl get pods
 # Step 4:
 # Forward the container port to a host
-  minikube kubectl port-forward pod/predictions-app --address 0.0.0.0 80:80
+# minikube kubectl port-forward pod/predictions-app --address 0.0.0.0 80:80
