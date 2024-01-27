316c36fc0043987d953e732284f6147118de19e4
TSundzukani <tsundzukani.mokgalaka@bcx.co.za>
2024-01-27 07:32:08 +0200

    Check cluster availability before deploy

---
+++ cluster_availability.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cluster_name="Capstone-EKS-Cluster"
+region="us-east-1"
+
+while true; do
+    status=$(aws eks describe-cluster --name $cluster_name --region $region | jq -r '.cluster.status')
+    if [ "$status" == "ACTIVE" ]; then
+        echo "Cluster is active"
+        break
+    fi
+    echo "Waiting for cluster to become active..."
+    sleep 20
+done
